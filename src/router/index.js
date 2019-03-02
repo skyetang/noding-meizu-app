f62c0ef1a173f590c89aa783449a5f3a55c513c4
skye <forbetter@yeah.net>
2019-03-02 10:32:07 +0800

    meizu project

---
+++ src/router/index.js
@@ -0,0 +1,39 @@
+import Vue from 'vue';
+import Router from 'vue-router';
+import Index from '../view/index';
+import Category from '../view/category';
+import Detail from '../view/detail';
+import Shopcart from '../view/shopcart';
+import Order from '../view/order';
+
+Vue.use(Router);
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'Index',
+      component: Index
+    },
+    {
+      path: '/category/:id?',
+      name: 'Category',
+      component: Category
+    },
+    {
+      path: '/detail/:id',
+      name: 'Detail',
+      component: Detail
+    },
+    {
+      path: '/shopcart',
+      name: 'Shopcart',
+      component: Shopcart
+    },
+    {
+      path: '/order',
+      name: 'Order',
+      component: Order
+    }
+  ]
+});
